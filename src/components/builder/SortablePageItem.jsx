65ce519b01f645a995f3744d0e6981358b882265
Shabnam <shabnam.ofl@gmail.com>
2026-07-24 12:09:21 +0530

    Add custom scrollbar to page item blocks list

--- src/components/builder/SortablePageItem.jsx
+++ src/components/builder/SortablePageItem.jsx
@@ -74,7 +74,7 @@ export const SortablePageItem = ({
                Skippable Page
              </label>
           </div>
-          <div className="p-1 flex flex-col gap-1 min-h-[50px] max-h-[300px] overflow-y-auto custom-scrollbar">
+          <div className="p-1 flex flex-col gap-1 min-h-[50px] max-h-[250px] overflow-y-auto custom-scrollbar">
           {page.blocks.length === 0 ? (
             <div className="text-xs font-bold text-gray-500 p-3 text-center">Empty Page</div>
           ) : (
